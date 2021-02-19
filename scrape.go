bc470565895e04e8c44b5a8c05299c0b4f60916b
Jess <jtrinh619@gmail.com>
2021-02-18 21:51:40 -0800

    [review] data successfully scraped but  format is appearing inconsistently

--- scrape.go
+++ scrape.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/gocolly/colly"
-	"regexp"
 	"io/ioutil"
-)
+	"regexp"
 
+	"github.com/gocolly/colly"
+)
 
 // AmazonResult struct that can be used for json
 type AmazonResult struct {
@@ -46,24 +46,46 @@ func writeFile(name string, data string) {
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
-	// Instantiate default collector; gives access to methods allowing 
+	// Instantiate default collector; gives access to methods allowing
 	// trigger callback functions when certain event happens
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	// 
+	// parse HTML
 	c.OnHTML("div.s-result-list.s-search-results.sg-row", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+		link := e.Attr("href")
 
-				// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+
+		// loop through products in the search result list
+		e.ForEach("div.a-section.a-spacing-medium", func(_ int, e *colly.HTMLElement) {
+			// access wanted values with css selectors
+			var productName, stars, price string
+
+			productName = e.ChildText("span.a-size-medium.a-color-base.a-text-normal")
+			if productName == "" {
+				// If no name then return and go directly to the next element
+				return
+			}
+
+			// [REVIEW] inconsistency found when getting stars and price
+			stars = e.ChildText("span.a-icon-alt")
+			formatStars(&stars)
+
+			price = e.ChildText("span.a-price > span.a-offscreen")
+			formatPrice(&price)
+
+			fmt.Printf("Product Name: %s \nStars: %s \nPrice: %s \n", productName, stars, price)
+		})
 	})
 
 	// Before making a request print "Visiting ..."
+	// request Amazon's result page
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://www.amazon.com/
+	// Start scraping on https://www.amazon.com
 	c.Visit("https://www.amazon.com/s?k=alpaca+plush&ref=nb_sb_noss_1")
 }
